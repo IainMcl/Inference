01c016c9154a20f17dd6812c540751cfd9503c6c
IainMcl <mclaughlan.iain@gmail.com>
2020-07-10 21:44:43 +0100

    Better UI and GUI ideas

---
+++ gui/gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+class GUI(object):
+    def __init__(self):
+        self.win = tk.Tk()
+        self.win.title("Iference")
+        self.win.geometry('500x500')
+        
+        self.window.mainloop()
